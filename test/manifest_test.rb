0109904f5b3c4db6ab40329214de41e213eda8f0
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-02-04 13:05:37 +0000

    Add test with empty manifest

--- test/manifest_test.rb
+++ test/manifest_test.rb
@@ -8,4 +8,12 @@ class DGDToolsTest < Minitest::Test
   def test_it_does_something_useless
     assert true
   end
+
+  def test_empty_precheck
+    Dir.chdir(File.join(__dir__, "data", "empty")) do
+      repo = DGD::Manifest::Repo.new
+      repo.manifest_file("dgd.manifest")
+      repo.precheck(".")
+    end
+  end
 end
