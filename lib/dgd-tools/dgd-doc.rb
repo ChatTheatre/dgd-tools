814cf556cde8ba3a15600baa2fa6ca405ba9ae29
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-03-31 22:35:43 +0100

    More work on the grammar

--- lib/dgd-tools/dgd-doc.rb
+++ lib/dgd-tools/dgd-doc.rb
@@ -2,6 +2,7 @@ require "dgd-tools/version"
 
 require "treetop"
 
+require_relative "dgd_grammar_support"
 Treetop.load "#{__dir__}/dgd_grammar.tt"
 
 # Preprocessor incantation: cpp -C -I. -D__DGD__ name_of_file.c
@@ -11,6 +12,8 @@ module DGD; end
 module DGD::Doc
 
   class SourceFile
+    attr_reader :parser
+
     def initialize(path, dgd_root:)
       unless File.exist?(path)
         raise "No such source file for DGD::Doc::Sourcefile: #{path.inspect}"
@@ -27,19 +30,27 @@ module DGD::Doc
 
     def parse_contents
       preproc_output = `cpp -C -I#{@dgd_root}/include -D__DGD__ #{@path}`
-      #preproc_output.gsub! /\n+/, "\n"  # The preprocessor often winds up leaving a lot of newlines from various sources
-
+      preproc_output.gsub!(/\n+/, "\n")  # The preprocessor often winds up leaving a lot of newlines from various sources
+      preproc_output.gsub!(/^# \d+ ".*"\w*\d*$/, "")
       data = @parser.parse(preproc_output)
-      unless data
-        if !data
-          puts @parser.failure_reason
-          puts @parser.failure_line
-          puts @parser.failure_column
+      if data
+        clean_tree(data)
+        puts data.inspect
+      else
+        puts @parser.failure_reason
+        puts @parser.failure_line
+        puts @parser.failure_column
 
-          puts "Preproc output was:#{preproc_output}\n\n\n"
-        end
+        puts "Preproc output was:#{preproc_output}\n\n\n"
         raise "Parse error!"
       end
     end
+
+    def clean_tree(root_node)
+      return if(root_node.elements.nil?)
+      root_node.elements.each {|node| self.clean_tree(node) }
+      root_node.elements.delete_if {|node| node.class.name == "Treetop::Runtime::SyntaxNode" && node.text_value.size < 20 }
+    end
+
   end
 end
--- test/parser_test.rb
+++ test/parser_test.rb
@@ -9,13 +9,26 @@ class DGDParserTest < Minitest::Test
     @skotos_dir = File.join(@dgd_source_dir, "skotos")
   end
 
+  def test_random_parse_bits
+    sf = DGD::Doc::SourceFile.new File.join(@dgd_source_dir, "inherit_test.c"), dgd_root: @dgd_source_dir
+    assert sf.parser.parse("varargs int clone", root: "formals")
+  end
+
   def test_inherits_only
-    sf = DGD::Doc::SourceFile.new(File.join(@dgd_source_dir, "inherit_test.c"), dgd_root: @dgd_source_dir)
+    DGD::Doc::SourceFile.new File.join(@dgd_source_dir, "inherit_test.c"), dgd_root: @dgd_source_dir
+  end
+
+  def test_functions
+    DGD::Doc::SourceFile.new File.join(@dgd_source_dir, "function_test.c"), dgd_root: @dgd_source_dir
   end
 
   def test_trivial_preprocessor
     # This file, if used alone, preprocesses to nothing since it just defines macros.
-    sf = DGD::Doc::SourceFile.new(File.join(@skotos_dir, "include/System.h"), dgd_root: @skotos_dir)
+    DGD::Doc::SourceFile.new File.join(@skotos_dir, "include/System.h"), dgd_root: @skotos_dir
   end
+
+  #def test_skotos_source_files
+  #  sf = DGD::Doc::SourceFile.new File.join(@skotos_dir, "lib/version.c"), dgd_root: @skotos_dir
+  #end
 
 end
