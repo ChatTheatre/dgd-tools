3071d315397be60b8e13a428648e6c6b4f2f42c9
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-03-31 10:01:25 +0100

    First successful parser tests

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -2,14 +2,14 @@ require "test_helper"
 require "dgd-tools/dgd-doc"
 
 class DGDParserTest < Minitest::Test
-    def initialize(*args)
-        super
-        @test_dir = __dir__
-        @dgd_source_dir = File.join(@test_dir, "dgd_source")
-    end
+  def initialize(*args)
+    super
+    @test_dir = __dir__
+    @dgd_source_dir = File.join(@test_dir, "dgd_source")
+  end
 
-    def test_that_it_has_a_version_number
-        refute_nil ::DGD::VERSION
-    end
+  def test_inherits_only
+    sf = DGD::Doc::SourceFile.new(File.join(@dgd_source_dir, "inherit_test.c"))
+  end
 
 end
